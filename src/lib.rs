3e92cd93aa13e20465ddafe258aa378d84bc9777
jhoudan <jerom.houdan@gmail.com>
2017-01-21 22:52:29 +0100

    [UPD] lib documentation

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,20 @@
 //!
 //! This crate is a wrapper around the open API of Recast.AI.
 //! It allows you to build bots and analyze text and audio files
+//!
+//! # Usage
+//! ```
+//! extern crate rustcastai;
+//!
+//! let client = rustcastai::Client::new("MY_REQUEST_ACCESS_TOKEN").unwrap();
+//!
+//! let res = client.text_request("Hello, world").unwrap();
+//! if let Some(intent) = res.intent() {
+//!     println!("Intent detected: {}", intent);
+//! } else {
+//!     println!("No intent detected");
+//! }
+//! ```
 
 extern crate curs;
 extern crate reqwest;
@@ -18,17 +32,17 @@ mod traits;
 /// Representation of the API resources (Action, Intent,...)
 pub mod resources;
 
+/// Responses the client method can sent you back (Request for /request and Converse for /converse)
 pub mod responses;
 
 /// Rustcastai Client to perform requests to the Recast.AI API
 pub mod client;
-pub use client::Client;
+#[doc(inline)] pub use client::Client;
 
-/// Rustcastai's error types
-pub mod error;
+mod error;
+pub use error::RecastError;
 
-/// Bunch of constants used in the SDK
-pub mod constants {
+mod constants {
     pub const REQUEST_ENDPOINT: &'static str = "https://api.recast.ai/v2/request";
     pub const CONVERSE_ENDPOINT: &'static str = "https://api.recast.ai/v2/converse";
 
